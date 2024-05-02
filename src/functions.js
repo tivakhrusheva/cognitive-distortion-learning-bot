0e0c78bbb67bd9c38110bc5b7ea3ff62e6b4c420
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-02 15:39:20 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,22 +1,29 @@
-function sendCard(context, object, i) {
+function sendCard(context, object, i, caption) {
     log("obj " + object);
     log("index " + i);
     context.response.replies = context.response.replies || [];
     if (i < Object.keys(object).length) {
-        context.response.replies.push({
-            "type": "image",
-            "imageUrl": object[Object.keys(object)[i]],
-            "imageName": "<u>" + Object.keys(object)[i] + "</u>",
-            "markup": "html"
-        })
-    }
+        if (!caption) {
+            context.response.replies.push({
+                "type": "image",
+                "imageUrl": object[Object.keys(object)[i]],
+                "imageName": "<u>" + Object.keys(object)[i] + "</u>",
+                "markup": "html"
+            })
+        }
+        else {
+            context.response.replies.push({
+                "type": "image",
+                "imageUrl": object[Object.keys(object)[i]]
+            })
+        }
     else {
-        context.response.replies.push({
-            "type": "text",
-            "text": "Вы изучили все искажения, которые мне известны! Время переходить к практике.\n Введите команду /train.",
-            "markup": "html"
-        })
-    }
+            context.response.replies.push({
+                "type": "text",
+                "text": "Вы изучили все искажения, которые мне известны! Время переходить к практике.\n Введите команду /train.",
+                "markup": "html"
+            })
+        }
 }
 
 
