a8851c1ba94c0439d553b142de6572bfc841a930
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-02 15:20:21 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -55,7 +55,8 @@ function sendMultipleCards(context, object1, object2, i) {
             {
             "type": "photo",
             "media": object1[Object.keys(object1)[i]],
-            "caption": "<u>" + Object.keys(object1)[i] + "</u>"},
+            "caption": "<u>" + Object.keys(object1)[i] + "</u>",
+            "parse_mode": "html"}
             {
             "type": "photo",
             "media": object2[Object.keys(object2)[i]],
