cab5cc12dc47f941a4066fa8f9c98992fb6181d0
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-02 18:03:14 +0000

    add new pictures

--- src/data/distortions.js
+++ src/data/distortions.js
@@ -1,23 +1,27 @@
 var urls = {
     "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/black-n-white-thinking.jpg",
-    "Фильтрация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/filtering.jpg",
+    "Негативный фильтр": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/filtering.jpg",
     "Сверхобобщение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/overgeneralization.jpg",
+    "Обесценивание": "",
     "Эмоциональное рассуждение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/emotional-argument.jpg",
     "Катастрофизация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/catastrophizing.jpg",
     "Персонализация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/personaliation.jpg",
     "Навешивание ярлыков": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/labeling.jpg",
     "Долженствование": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/should-n-must-thinking.jpg",
-    "Чтение мыслей": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/mind-reading.jpg"
+    "Чтение мыслей": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/mind-reading.jpg",
+    "Иллюзия контроля": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/control.jpg"
 }
 
 var urls_solutions = {
-    "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/black-n-white-thinking.jpg",
-    "Фильтрация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/filtering.jpg",
-    "Сверхобобщение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/overgeneralization.jpg",
-    "Эмоциональное рассуждение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/emotional-argument.jpg",
-    "Катастрофизация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/catastrophizing.jpg",
-    "Персонализация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/personaliation.jpg",
-    "Навешивание ярлыков": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/labeling.jpg",
-    "Долженствование": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/should-n-must-thinking.jpg",
-    "Чтение мыслей": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/mind-reading.jpg"
+    "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/bw-advice.jpg",
+    "Негативный фильтр": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/neg-advice.jpg",
+    "Сверхобобщение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/over-advice.jpg",
+    "Обесценивание": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/deprec-advice.jpg",
+    "Эмоциональное рассуждение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/em-advice.jpg",
+    "Катастрофизация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/cat-advice.jpg",
+    "Персонализация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/person-advice.jpg",
+    "Навешивание ярлыков": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/lab-advice.jpg",
+    "Долженствование": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/should-advice.jpg",
+    "Чтение мыслей": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/mind-advice.jpg",
+    "Иллюзия контроля": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/control-cat.jpg"
 }
