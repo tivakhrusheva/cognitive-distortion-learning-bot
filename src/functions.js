fa1dbe9174773406c4c70e5596ddf4860b07f0bd
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-11 00:25:40 +0000

    remove old version of buttons

--- src/functions.js
+++ src/functions.js
@@ -54,7 +54,6 @@ function sendMultipleCards(context, object1, object2, i) {
             {
             "type": "photo",
             "media": object1[Object.keys(object1)[i]],
-            // "caption": "<b>" + Object.keys(object1)[i] + "</b>",
             "parse_mode": "html"},
             {
             "type": "photo",
