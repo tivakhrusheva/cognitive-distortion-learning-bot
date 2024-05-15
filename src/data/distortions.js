0c81cc5b7793982d5ece6430f78b00002e8f2a20
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-15 14:12:01 +0000

    Edited files:     /src/data/distortions.js

--- src/data/distortions.js
+++ src/data/distortions.js
@@ -2,7 +2,7 @@ var urls = {
     "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/black-n-white-thinking.jpg",
     "Негативный фильтр": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/filtering.jpg",
     "Сверхобобщение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/overgeneralization.jpg",
-    "Обесценивание": "",
+    "Обесценивание положительного": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/discounting_the_positive.jpg",
     "Эмоциональное рассуждение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/emotional-argument.jpg",
     "Катастрофизация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/catastrophizing.jpg",
     "Персонализация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/personaliation.jpg",
@@ -13,10 +13,10 @@ var urls = {
 }
 
 var urls_solutions = {
-    "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/bw-advice.jpg",
+    "Черно-белое мышление": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/bw-thinking-advice.jpg",
     "Негативный фильтр": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/neg-advice.jpg",
     "Сверхобобщение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/over-advice.jpg",
-    "Обесценивание": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/deprec-advice.jpg",
+    "Обесценивание положительного": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/discounting_the_positive_recommendation.jpg",
     "Эмоциональное рассуждение": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/em-advice.jpg",
     "Катастрофизация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/cat-advice.jpg",
     "Персонализация": "https://storage.yandexcloud.net/test-bucket-for-uxers/cogdis-pictures/advice/person-advice.jpg",
