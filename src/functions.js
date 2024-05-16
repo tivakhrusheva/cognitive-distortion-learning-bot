1be10f6a14bc8c218bb7a9d541801e09ab80b55c
vahruscheva.tanya@gmail.com <vahruscheva.tanya@gmail.com>
2024-05-16 19:41:17 +0000

    Edited files:     /src/Distortion.sc     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -62,7 +62,6 @@ function sendInlineButtonsWIndeces(context, buttonsNames) {
 
 function sendMultipleCards(context, object1, object2, i) {
     context.response.replies = context.response.replies || [];
-    }
     if (i < Object.keys(object1).length) {
         context.response.replies.push({
           "type": "raw",
